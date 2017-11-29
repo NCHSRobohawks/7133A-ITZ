a0ac5d19d7c72388920a10e5c39fb952e1f686db
Tanveer Mittal <tanveer@mittals.us>
2017-11-29 08:08:26 -0600

    Finalized pid tuning of all components

--- PID_Auton_Template.c
+++ PID_Auton_Template.c
@@ -1,16 +1,15 @@
-#pragma config(I2C_Usage, I2C1, i2cSensors)
+
 #pragma config(Sensor, in1,    mobile1,        sensorPotentiometer)
 #pragma config(Sensor, in2,    mobile2,        sensorPotentiometer)
-#pragma config(Sensor, in3,    gyro,           sensorGyro)
-#pragma config(Sensor, in4,    bar,            sensorPotentiometer)
-#pragma config(Sensor, dgtl1,  elevator,       sensorQuadEncoder)
+#pragma config(Sensor, dgtl1,  left,           sensorQuadEncoder)
+#pragma config(Sensor, dgtl3,  right,          sensorQuadEncoder)
 #pragma config(Sensor, dgtl6,  claw,           sensorDigitalOut)
-#pragma config(Sensor, I2C_1,  ,               sensorQuadEncoderOnI2CPort,    , AutoAssign )
-#pragma config(Sensor, I2C_2,  ,               sensorQuadEncoderOnI2CPort,    , AutoAssign )
+#pragma config(Sensor, dgtl7,  elevator,       sensorQuadEncoder)
+#pragma config(Sensor, dgtl11, bar,            sensorQuadEncoder)
 #pragma config(Motor,  port1,           bar1,          tmotorVex393_HBridge, openLoop)
-#pragma config(Motor,  port2,           rf,            tmotorVex393_MC29, openLoop, encoderPort, I2C_1)
+#pragma config(Motor,  port2,           rf,            tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port3,           rb,            tmotorVex393_MC29, openLoop, reversed)
-#pragma config(Motor,  port4,           lf,            tmotorVex393_MC29, openLoop, reversed, encoderPort, I2C_2)
+#pragma config(Motor,  port4,           lf,            tmotorVex393_MC29, openLoop, reversed)
 #pragma config(Motor,  port5,           lb,            tmotorVex393_MC29, openLoop, reversed)
 #pragma config(Motor,  port6,           goal2,         tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port7,           goal1,         tmotorVex393_MC29, openLoop, reversed)
@@ -22,7 +21,8 @@
 float kP[4], kI[4], kD[4], kL[4];
 long tolerance[4];
 long ticksPerRotation, ticksPerFoot, waitBetweenPID;
-bool turning = False;
+bool turning = false;
+long degrees = 0;
 /*
 Constants
 * * *
@@ -32,13 +32,8 @@ Constants
 3 - Chain bar
 * * *
 */
+
 void init() {
-	SensorType[in3] = sensorNone;
-	wait1Msec(1000);
-	SensorType[in3] = sensorGyro;
-	wait1Msec(2000);
-	SensorScale[in8] = 260;
-	SensorFullCount[in8] = 3600;
     //Wheel base constants
     kP[0] = -0.08;
     kI[0] = -0.3;
@@ -63,9 +58,9 @@ void init() {
     tolerance[2] = 0;
 
     //Chain bar constants
-    kP[3] = 0.0;
-    kI[3] = 0.0;
-    kD[3] = 0.0;
+    kP[3] = 10;
+    kI[3] = 4;
+    kD[3] = 2;
     kL[3] = 0.0;
     tolerance[3] = 0;
 
@@ -75,10 +70,18 @@ void init() {
 
     waitBetweenPID = 1000; // Number of milliseconds to wait after each PID move
 
+    SensorType[in3] = sensorNone;
+    wait1Msec(1000);
+    SensorType[in3] = sensorGyro;
+    wait1Msec(2000);
+    //SensorScale[in3] = 260;
+    SensorFullCount[in3] = 3600;
+
+
 }
 
 long target[6] = {0, 0, sensorValue(mobile1), sensorValue(mobile2), 0, 0};
-long degrees = 0;
+
 /*
 Targets
 * * *
@@ -99,60 +102,57 @@ task pid() {
     long d[6] = {0, 0, 0, 0, 0, 0};
 
     while(true) {
-	if(!turning){
-		error[0] = target[0] - SensorValue(left);
-		error[1] = target[1] - SensorValue(right);
-		p[0] = error[0];
-		p[1] = error[1];
-		i[0] = abs(i[0] + error[0]) < kL[0] ? i[0] + error[0] : sgn(i[0] + error[0])*kL[0];
-		i[1] = abs(i[1] + error[1]) < kL[0] ? i[1] + error[1] : sgn(i[1] + error[1])*kL[0];
-		d[0] = error[0] - pError[0];
-		displayLCDNumber(0,1, d[0]);
-		d[1] = error[1] - pError[1];
-		motor[port4] = p[0]*kP[0] + i[0]*kI[0] + d[0]*kD[0];
-		motor[port5] = p[0]*kP[0] + i[0]*kI[0] + d[0]*kD[0];
-		motor[port2] = p[1]*kP[0] + i[1]*kI[0] + d[1]*kD[0];
-		motor[port3] = p[1]*kP[0] + i[1]*kI[0] + d[1]*kD[0];
-    }
-	else{
-		error[0] = degrees - SensorValue(in3);
-		error[1] = degrees - SenosrValue(in3);
-		p[0] = error[0];
-		p[1] = error[1];
-		i[0] = abs(i[0] + error[0]) < kL[0] ? i[0] + error[0] : sgn(i[0] + error[0])*kL[0];
-		i[1] = abs(i[1] + error[1]) < kL[0] ? i[1] + error[1] : sgn(i[1] + error[1])*kL[0];
-		d[0] = error[0] - pError[0];
-		displayLCDNumber(0,1, d[0]);
-		d[1] = error[1] - pError[1];
-		motor[port4] = p[0]*kP[0] + i[0]*kI[0] + d[0]*kD[0];
-		motor[port5] = p[0]*kP[0] + i[0]*kI[0] + d[0]*kD[0];
-		motor[port2] = p[1]*kP[0] + i[1]*kI[0] + d[1]*kD[0];
-		motor[port3] = p[1]*kP[0] + i[1]*kI[0] + d[1]*kD[0];
-	}
+				if(turning){
+	        error[0] = degrees - SensorValue(in3);
+	        p[0] = error[0];
+	        i[0] = abs(i[0] + error[0]) < kL[0] ? i[0] + error[0] : sgn(i[0] + error[0])*kL[0];
+	        d[0] = error[1] - pError[1];
+	        motor[port4] = -(p[0]*-0.095 + i[0]*-0.73 + d[0]*0);
+	        motor[port5] = -(p[0]*-0.095 + i[0]*-0.73 + d[0]*0);
+	        motor[port2] = p[0]*-0.095 + i[0]*-0.73 + d[0]*0;
+	        motor[port3] = p[0]*-0.095 + i[0]*-0.73 + d[0]*0;
+      }
+      	else{
+      		error[0] = target[0] - SensorValue(left);
+	        error[1] = target[1] - SensorValue(right);
+	        p[0] = error[0];
+	        p[1] = error[1];
+	        i[0] = abs(i[0] + error[0]) < kL[0] ? i[0] + error[0] : sgn(i[0] + error[0])*kL[0];
+	        i[1] = abs(i[1] + error[1]) < kL[0] ? i[1] + error[1] : sgn(i[1] + error[1])*kL[0];
+	        d[1] = error[1] - pError[1];
+	        d[2] = error[2] - pError[2];
+	        motor[port4] = p[0]*kP[0] + i[0]*kI[0] + d[0]*kD[0];
+	        motor[port5] = p[0]*kP[0] + i[0]*kI[0] + d[0]*kD[0];
+	        motor[port2] = p[1]*kP[0] + i[1]*kI[0] + d[1]*kD[0];
+	        motor[port3] = p[1]*kP[0] + i[1]*kI[0] + d[1]*kD[0];
+	      }
+
         error[2] = target[2] - SensorValue(mobile1);
         error[3] = target[3] - SensorValue(mobile2);
-        error[4] = target[4] - SensorValue(bar);
-        error[5] = target[5] - SensorValue(elevator);
+        error[4] = target[4] - SensorValue(elevator);
+        error[5] = target[5] - SensorValue(bar);
+
         p[2] = error[2];
         p[3] = error[3];
         p[4] = error[4];
         p[5] = error[5];
+
         i[2] = abs(i[2] + error[2]) < kL[0] ? i[2] + error[2] : sgn(i[2] + error[2])*kL[0];
         i[3] = abs(i[3] + error[3]) < kL[0] ? i[3] + error[3] : sgn(i[3] + error[3])*kL[0];
         i[4] = abs(i[4] + error[4]) < kL[0] ? i[4] + error[4] : sgn(i[4] + error[4])*kL[0];
         i[5] = abs(i[5] + error[5]) < kL[0] ? i[5] + error[5] : sgn(i[5] + error[5])*kL[0];
-        
-        d[2] = error[2] - pError[2];
+        d[0] = error[0] - pError[0];
+        //displayLCDNumber(0,1, d[0]);
+				d[2] = error[3] - pError[3];
         d[3] = error[3] - pError[3];
         d[4] = error[4] - pError[4];
         d[5] = error[5] - pError[5];
-       
         motor[port7] = p[2]*kP[1] + i[2]*kI[1] + d[2]*kD[1];
         motor[port6] = p[3]*kP[1] + i[3]*kI[1] + d[3]*kD[1];
         motor[port8] = p[4]*kP[2] + i[4]*kI[2] + d[4]*kD[2];
         motor[port9] = p[4]*kP[2] + i[4]*kI[2] + d[4]*kD[2];
-        //motor[port1] = p[5]*kP[3] + i[5]*kI[3] + d[5]*kD[3];
-        //motor[port10] = p[5]*kP[3] + i[5]*kI[3] + d[5]*kD[3];
+        motor[port1] = p[5]*kP[3] + i[5]*kI[3] + d[5]*kD[3];
+        motor[port10] = p[5]*kP[3] + i[5]*kI[3] + d[5]*kD[3];
 
         pError[0] = error[0];
         pError[1] = error[1];
@@ -165,43 +165,68 @@ task pid() {
     }
 
 }
-task deploy(){
-		motor[port1] = 100;
-		motor[port10] = 100;
-		wait1Msec(2000);
-		motor[1] = 127;
-		motor[port1] = 0;
-		motor[port10] = 0;
-	}
+
+int reverse = 1;
+int half = 1;
+int rl=0;
+int hl=0;
+int sl=0;
+int ll=0
+bool lower = False;
+long error[2] = {0, 0};
+long i[2] = {0,0};
+
+void mogo_forward(int inches){
+	SensorValue[right] = 0;
+	SensorValue[left] = 0;
+	target[0] = target[1] = inches*ticksPerFoot/12;
+	while(abs(SensorValue(left) - target[0]) > tolerance[0] && abs(SensorValue(right) - target[1]) > tolerance[0]);
+
+}
+
+void forward(int inches){
+	SensorValue[right] = 0;
+	SensorValue[left] = 0;
+	target[0] = target[1] = -inches*ticksPerFoot/12;
+	while(abs(SensorValue(left) - target[0]) > tolerance[0] && abs(SensorValue(right) - target[1]) > tolerance[0]);
+}
+
+void turn(int degrees10){
+	turning = True;
+	degrees += degrees10;
+	while(True);
+	//while(abs(SensorValue(in3) - degrees) > 10);
+	//turning = False;
+}
+void arm_raise(){
+	target[5] = 60;
+}
+void mogo_lower() {
+	target[2] = 730;
+	target[3] = 345;
+}
+
+task red_left_far(){
+  SensorValue[claw] = 1;
+  //startTask(deploy);
+  wait1Msec(500);
+  startTask(pid);
+  mogo_lower();
+  while(abs(SensorValue(mobile1) - target[2]) > tolerance[3]);
+  wait1Msec(waitBetweenPID);
+	mogo_forward(47);
+  while(abs(nMotorEncoder[port4] - target[0]) > tolerance[0] && abs(nMotorEncoder[port2] - target[1]) > tolerance[0]);
+}
+
 
 task main() {
-
-    init();
-    SensorValue[claw] = 1;
-    startTask(deploy);
-    wait1Msec(500);
-    startTask(pid);
-/*
-    target[0] += -24*ticksPerFoot/12;
-    target[1] += -24*ticksPerFoot/12;
-    displayLCDNumber(0,0,target[0]);
-*/
-
-    target[2] = 730;
-    target[3] = 345;
-    while(abs(SensorValue(mobile1) - target[2]) > tolerance[3]);
-    wait1Msec(waitBetweenPID);
-    target[0] += -47*ticksPerFoot/12;
-    target[1] += -47*ticksPerFoot/12;
-	while(abs(SensorValue(left) - target[0]) > tolerance[0] && abs(SensorValue(right) - target[1]) > tolerance[0]);
-    /*
-    target[0] += 19*ticksPerFoot/12;
-    target[1] += 19*ticksPerFoot/12;
-    while(abs(nMotorEncoder[port4] - target[0]) > tolerance[0] && abs(nMotorEncoder[port2] - target[1]) > tolerance[0]);
-    wait1Msec(waitBetweenPID);
-    target[2] = 3900;
-    while(abs(SensorValue(mobile) - target[2]) > tolerance[3]);
-
-    stopTask(pid);'*/
-
+	sensorValue[bar] = 0;
+	init();
+	startTask(pid);
+	SensorValue[claw] = 1;
+	arm_raise();
+	wait1Msec(waitBetweenPID);
+	mogo_lower();
+	mogo_forward(50);
+	while(True);
 }
